b5f97fd3a877bb65c8a54fb0104882d7cf4fbd06
Naomi0410 <naomialicia15@gmail.com>
2025-11-01 06:14:07 +0100

    increase rate limit

--- backend/middlewares/rateLimit.js
+++ backend/middlewares/rateLimit.js
@@ -1,9 +1,14 @@
 import { rateLimit } from "express-rate-limit";
 
 export const limitRequests = rateLimit({
-  windowMs: 10 * 10 * 1000, //100secs
-  max: 5, //limit to 5 requests per 100secs
+  windowMs: 15 * 60 * 1000, // 15 minutes
+  max: 100, // limit each IP to 100 requests per windowMs
+  message: "Too many requests from this IP, please try again after 15 minutes",
   statusCode: 429,
+  headers: true, // Return rate limit info in headers
+  // Customize headers if needed
+  legacyHeaders: false, // Disable the `X-RateLimit-*` headers
+  standardHeaders: true, // Return rate limit info in the `RateLimit-*` headers
 });
 
 export default limitRequests;
